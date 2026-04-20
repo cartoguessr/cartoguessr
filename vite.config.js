63781972c4ec171d1e7f486b3435514cca2d5824
Vedh Ramesh <vedh@ist2-13049.apn.wlan.private.upenn.edu>
2026-04-20 14:07:35 -0400

    fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'cartoguessr' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/cartoguessr/',
+  base: '/',
 })
